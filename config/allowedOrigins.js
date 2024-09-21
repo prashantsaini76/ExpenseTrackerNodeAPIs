a0e8ae96fef3ca91588ea74d495615315cc7ca9b
prashantsaini76 <prashantsaini76@gmail.com>
2024-09-21 18:20:03 +0530

    added railway origin for CORS

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -3,7 +3,8 @@ const allowedOrigins = [
     'http://127.0.0.1:5500',
     'http://localhost:3500',
     'http://localhost:3000',
-    'https://easyexpensemeter.netlify.app'
+    'https://easyexpensemeter.netlify.app',
+    'https://expensemeter.up.railway.app'
 ];
 
 module.exports = allowedOrigins;
